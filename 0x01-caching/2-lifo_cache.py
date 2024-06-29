b0b8c532667bf13e35484972e4f620ec4c98db29
Isaac Ndirangu Muturi <ndirangumuturi749@gmail.com>
2024-06-29 15:14:10 +0300

    Update 2-lifo_cache.py

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" LIFOCache module
+"""
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """ LIFOCache defines:
+      - LIFO caching system
+    """
+
+    def __init__(self):
+        """ Initialize
+        """
+        super().__init__()
+        self.last_key = None
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key is None or item is None:
+            return
+
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            if key not in self.cache_data:
+                if self.last_key:
+                    print(f"DISCARD: {self.last_key}")
+                    del self.cache_data[self.last_key]
+
+        self.cache_data[key] = item
+        self.last_key = key
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        return self.cache_data.get(key)
