a52338026837c07354255d6ddd07b787dc3c25f1
Isaac Ndirangu Muturi <ndirangumuturi749@gmail.com>
2024-07-02 13:29:57 +0300

    Update 0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -6,10 +6,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET'], strict_slashes=False)
 def index():
     """hello world"""
     return render_template('0-index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
