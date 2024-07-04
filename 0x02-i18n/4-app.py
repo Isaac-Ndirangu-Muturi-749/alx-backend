9b214812ac6775a672146ab06ab17de210c7093c
Isaac Ndirangu Muturi <ndirangumuturi749@gmail.com>
2024-07-04 11:45:35 +0300

    update task 5

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """ 4-app module"""
 
 from flask import Flask, render_template, request
--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,22 +1,13 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """ 5-app module"""
 
+
 from flask import Flask, render_template, request, g
-from flask_babel import Babel, _
-
-
-class Config:
-    """Configuration for Flask-Babel with available languages and defaults."""
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
-
+from flask_babel import Babel
 
 app = Flask(__name__)
-app.config.from_object(Config)
-
 babel = Babel(app)
-
+"""Babel object"""
 users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
     2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
@@ -25,14 +16,43 @@ users = {
 }
 
 
+class Config(object):
+    """Configuration for Flask-Babel with available languages and defaults."""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+"""class config"""
+
+
+@app.route('/')
+def root():
+    """Render the index page with localized messages."""
+    return render_template("5-index.html")
+
+
+@babel.localeselector
+def get_locale():
+    """Determine the best match for supported languages,
+      or use URL parameter."""
+    localLang = request.args.get('locale')
+    supportLang = app.config['LANGUAGES']
+    if localLang in supportLang:
+        return localLang
+    else:
+        return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
 def get_user():
     """Returns a user dictionary or None if the ID is not
       found or not passed."""
-    login_as = request.args.get('login_as')
-    if login_as:
-        user_id = int(login_as)
-        return users.get(user_id)
-    return None
+    try:
+        userId = request.args.get('login_as')
+        return users[int(userId)]
+    except Exception:
+        return None
 
 
 @app.before_request
@@ -41,23 +61,5 @@ def before_request():
     g.user = get_user()
 
 
-@babel.localeselector
-def get_locale():
-    """Determine the best match for supported languages,
-      or use URL parameter."""
-    locale = request.args.get('locale')
-    if locale in app.config['LANGUAGES']:
-        return locale
-    if g.user and g.user['locale'] in app.config['LANGUAGES']:
-        return g.user['locale']
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
-@app.route('/')
-def index():
-    """Render the index page with localized messages."""
-    return render_template('5-index.html')
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run()
