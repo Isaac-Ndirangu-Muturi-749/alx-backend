1d6bb1dcfce366597130d8d4501ca3b8482e13ae
Isaac Ndirangu Muturi <ndirangumuturi749@gmail.com>
2024-07-04 11:38:56 +0300

    task 4

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -14,17 +14,22 @@ class Config:
 
 app = Flask(__name__)
 app.config.from_object(Config)
+"""class config"""
+
 
 babel = Babel(app)
+"""Babel object"""
 
 
 @babel.localeselector
 def get_locale():
-    """Determine the best match for supported languages, or use URL parameter."""
-    locale = request.args.get('locale')
-    if locale in app.config['LANGUAGES']:
-        return locale
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    """ to determine the best match with our supported languages """
+    localLang = request.args.get('locale')
+    supportLang = app.config['LANGUAGES']
+    if localLang in supportLang:
+        return localLang
+    else:
+        return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
